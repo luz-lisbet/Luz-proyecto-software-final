2f3235a4b0f941449f4dbca53948174c59d35e39
Luz <cabiaadrianol@gmail.com>
2024-03-26 12:08:00 -0500

    segundo

--- database/migrations/2024_03_26_152050_create_resenas_table.php
+++ database/migrations/2024_03_26_152050_create_resenas_table.php
@@ -4,24 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateResenasTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('resenas', function (Blueprint $table) {
             $table->id();
+            $table->string('contenido');
+            $table->unsignedBigInteger('usuario_id');
+            $table->foreign('usuario_id')->references('id')->on('usuarios')->onDelete('cascade');
+            $table->unsignedBigInteger('libro_id');
+            $table->foreign('libro_id')->references('id')->on('libros')->onDelete('cascade');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('resenas');
     }
-};
+}
